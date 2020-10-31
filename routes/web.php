6d9fd8f92b83221b78ccaa9ab52e5e7d1af84ede
Kirill Kalashnikov <kirill310587@mail.ru>
2020-10-31 06:41:14 +0000

    Added tag and tag_task migrations

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,12 @@ Route::post('/tasks/{task}/steps', 'TaskStepsController@store');
 Route::post('/completed-steps/{step}', 'CompletedStepsController@store');
 Route::delete('/completed-steps/{step}', 'CompletedStepsController@destroy');
 
-//Route::get('/tasks', 'TasksController@index')->name('tasks.index');
-//Route::get('/tasks/create', 'TasksController@create')->name('tasks.create');
-//Route::post('/tasks','TasksController@store')->name('tasks.store');
-//Route::get('/tasks/{task}', 'TasksController@show')->name('tasks.show');
-//Route::get('/tasks/{task}/edit','TasksController@edit' )->name('tasks.edit');
-//Route::patch('/tasks/{task}', 'TasksController@update')->name('tasks.update');
-//Route::delete('/tasks/{task}', 'TasksController@destroy')->name('tasks.delete');
-//
+//tasks
+
+//tags
+
+
+
 Route::get('/', 'PostsController@index')->name('posts.index');
 Route::view('/about', 'about')->name('about');
 Route::get('/contacts', 'FeedbacksController@create')->name('feedbacks.create');
