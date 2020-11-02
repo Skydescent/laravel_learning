5158f825e13229198cc6fa685fd9a09fb5db684f
Kirill Kalashnikov <kirill310587@mail.ru>
2020-11-02 18:43:49 +0000

    Added pushall config into service.php

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Блог - @yield('title')</title>
+    <title>{{ config('app.name', '123456') }}</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
