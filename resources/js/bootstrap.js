bd2482c59f72d11d6716b47021ef27f195c1aac6
Kirill Kalashnikov <kirill310587@mail.ru>
2021-04-07 14:00:56 +0700

    Added Laravel Sail and npm packages for echo-server

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,10 +30,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 import Echo from 'laravel-echo';
+window.io = require('socket.io-client');
 
 window.Echo = new Echo({
     broadcaster: 'socket.io',
-    host: window.location.hostname + ':6001',
-    namespace: 'App\\Events',
-    authEndpoint: '/custom/to/auth'
+    host: window.location.hostname + ':6001'
 });
